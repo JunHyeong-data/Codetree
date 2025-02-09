6b5bbcf9588d20b27ff1cbb4450c70cd81f04c01
STATWEBDREAMER <parkjune0310@naver.com>
2025-02-09 12:26:12 +0900

    Title: 별표 출력하기 6, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250209/별표 출력하기 6/print-star-6.py
+++ 250209/별표 출력하기 6/print-star-6.py
@@ -1,13 +1,14 @@
 n = int(input())
+
 for i in range(n):
-    for j in range(i):
+    for _ in range(i):
         print(' ', end=' ')
-    for j in range(2*(n-i-1)+1):
+    for _ in range(2*(n-i)-1):
         print('*', end=' ')
     print()
-for i in range(n-1):
-    for j in range(2*(n-1-i)-2):
-        print('', end=' ')
-    for j in range(2*(i+1)+1):
+for i in range(n-2,-1,-1):
+    for _ in range(i):
+        print(' ', end=' ')
+    for _ in range(2*(n-i)-1):
         print('*', end=' ')
     print()
