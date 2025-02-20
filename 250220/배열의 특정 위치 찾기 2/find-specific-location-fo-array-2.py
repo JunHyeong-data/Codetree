505f942e591abcb3f359ee10beb7a6862ff8223c
STATWEBDREAMER <parkjune0310@naver.com>
2025-02-20 21:58:10 +0900

    Title: 배열의 특정 위치 찾기 2, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

--- 250220/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.py
+++ 250220/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.py
@@ -1,7 +1,7 @@
 arr = list(map(int, input().split()))
 
-result1 = arr[0:9:2]
-result2 = arr[1:9:2]
+result1 = arr[::2]
+result2 = arr[1::2]
 
 if sum(result1) > sum(result2):
     print(sum(result1) - sum(result2))
