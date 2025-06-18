4b42661fb942042964dc1bb256d0cc1d12209740
박준형 <parkjune0310@naver.com>
2025-06-19 00:45:46 +0900

    Title: 정상적인 구구단, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250619/정상적인 구구단/normal-multiple.py
+++ 250619/정상적인 구구단/normal-multiple.py
@@ -1,6 +1,7 @@
 N = int(input())
 
 for i in range(1, N + 1):  # 1단부터 N단까지
+    line_results = []
     for j in range(1, N + 1):  # 각 단의 1부터 N까지 곱셈
-        print(f"{i} * {j} = {i * j}")
-    print() # 각 단이 끝날 때마다 빈 줄 출력 (구분선 역할)
+        line_results.append(f"{i} * {j} = {i * j}")
+    print(", ".join(line_results)) # 각 단의 결과를 ', '로 연결하여 한 줄에 출력
