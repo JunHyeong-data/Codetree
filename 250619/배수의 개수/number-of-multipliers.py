f15018bbb076f99ec7c97cb99392a7570d459b78
박준형 <parkjune0310@naver.com>
2025-06-19 00:31:37 +0900

    Title: 배수의 개수, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250619/배수의 개수/number-of-multipliers.py
+++ 250619/배수의 개수/number-of-multipliers.py
@@ -1,13 +1,13 @@
-# 두 정수 A, B를 공백으로 구분하여 입력받습니다.
-A, B = map(int, input().split())
+# 변수 선언, 입력
+cnt3, cnt5 = 0, 0
 
-# 합을 저장할 변수를 초기화합니다.
-total_sum = 0
+for _ in range(10):
+	a = int(input())
+	
+	if a % 3 == 0:
+		cnt3 += 1
+	if a % 5 == 0:
+		cnt5 += 1
 
-# A부터 B까지 반복하며 홀수의 합을 구합니다.
-for i in range(A, B + 1):
-    if i % 2 != 0:  # i가 홀수이면
-        total_sum += i
-
-# 홀수의 합을 출력합니다.
-print(total_sum)
+# 출력
+print(cnt3, cnt5)
