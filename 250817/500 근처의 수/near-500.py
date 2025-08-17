24d68053187c721625b45643da4b8f56e4c28e3c
박준형 <parkjune0310@naver.com>
2025-08-17 22:44:12 +0900

    Title: 500 근처의 수, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250817/500 근처의 수/near-500.py
+++ 250817/500 근처의 수/near-500.py
@@ -1,13 +1,13 @@
 arr = list(map(int, input().split()))
 
-num1 = -1
-num2 = 1001
+max_val = -1
+min_val = 1001
 
-for i in range(len(arr)):
-    if arr[i] < 500 and arr[i] > num1:
-        num1 = arr[i]
-for j in range(len(arr)):
-    if arr[j] > 500 and arr[j] < num2:
-        num2 = arr[j]
-print(num1, num2)
+for elem in arr:
+    if elem < 500 and elem > max_val:
+        max_val = elem
+    if elem > 500 and elem < min_val:
+        min_val = elem
+
+print(max_val, min_val)
 
