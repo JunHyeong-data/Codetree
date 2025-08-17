bdba2cbcb4300449d12a9180fd49a707c3765131
박준형 <parkjune0310@naver.com>
2025-08-17 23:50:58 +0900

    Title: 배열의 합, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250817/배열의 합/sum-of-array.py
+++ 250817/배열의 합/sum-of-array.py
@@ -1,5 +1,7 @@
+arr_2d = [list(map(int, input().split())) for _ in range(4)]
 
-
-for _ in range(4):
-    arr = list(map(int, input().split()))
-    print(sum(arr))
+for i in range(4):
+    ans = 0
+    for j in range(4):
+        ans += arr_2d[i][j]
+    print(ans)
