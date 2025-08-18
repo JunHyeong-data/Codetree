3fd0b26479fadc0208a2b3f0fe6ca3605118eb2f
박준형 <parkjune0310@naver.com>
2025-08-18 23:00:16 +0900

    Title: 두 배열의 곱, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250818/두 배열의 곱/multiple-of-two-arrays.py
+++ 250818/두 배열의 곱/multiple-of-two-arrays.py
@@ -1,16 +1,12 @@
-arr1 = []
-while len(arr1) < 3:
-    line = input().strip()
-    if line:  # 빈 줄 아니면 추가
-        arr1.append(list(map(int, line.split())))
+arr1 = [list(map(int, input().split())) for _ in range(3)]
+input()
+arr2 = [list(map(int, input().split())) for _ in range(3)]
+arr3 = [[0 for _ in range(3)] for _ in range(3)]
 
-arr2 = []
-while len(arr2) < 3:
-    line = input().strip()
-    if line:
-        arr2.append(list(map(int, line.split())))
 
-arr3 = [[arr1[i][j] * arr2[i][j] for j in range(3)] for i in range(3)]
+for i in range(3):
+    for j in range(3):
+        arr3[i][j] = arr1[i][j] * arr2[i][j]
 
 for row in arr3:
     print(*row)
