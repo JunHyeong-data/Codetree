0e7782d7b9b60fb6d53cd2f20a8283836ac0eaa9
박준형 <parkjune0310@naver.com>
2025-08-19 18:55:45 +0900

    Title: 대각선으로 정수 채우기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250819/대각선으로 정수 채우기/diagonal-numbering.py
+++ 250819/대각선으로 정수 채우기/diagonal-numbering.py
@@ -1,16 +1,21 @@
-N, M = map(int, input().split()) # N과 M을 입력받음
-matrix = [[0] * M for _ in range(N)] # 0으로 채워진 N x M 행렬 생성
-num = 1 # 채울 숫자 초기화
+n, m = map(int, input().split())
 
-# 대각선 합 i+j의 값인 k를 기준으로 반복
-for k in range(N + M - 1):
-    start_i = max(0, k - M + 1)
-    end_i = min(k + 1, N)
-    for i in range(start_i, end_i):
-        j = k - i
-        matrix[i][j] = num
-        num += 1
+# Please write your code here.
 
-# 결과 출력
-for row in matrix:
-    print(*row)
+numlist = [[0] * m for _ in range(n)]
+
+cnt = 0
+
+for k in range(n+m-1):
+    for i in range(n):
+        for j in range(m):
+            if i + j == k:
+                cnt += 1
+                numlist[i][j] = cnt
+                
+
+for row in numlist:
+    for elem in row:
+        print(elem, end=" ")
+    print()
+
