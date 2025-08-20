5fab50040e6b1ef3f7b8a745e089f795fab7b9c9
박준형 <parkjune0310@naver.com>
2025-08-20 16:25:44 +0900

    Title: 공백을 기준으로 출력, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250820/공백을 기준으로 출력/output-based-on-space.py
+++ 250820/공백을 기준으로 출력/output-based-on-space.py
@@ -1,17 +1,8 @@
-# 두 줄 입력 받기
 line1 = input()
 line2 = input()
 
-# 두 문자열 합치기
-combined_string = line1 + line2
+combine = line1 + line2
 
-# 공백이 제거된 문자열을 저장할 변수 초기화
-result_string = ''
+result = combine.replace(' ', '')
 
-# 합쳐진 문자열을 하나씩 반복하면서 확인
-for char in combined_string:
-    if char != ' ':  # 공백이 아니면
-        result_string += char # 새 문자열에 추가
-
-# 결과 출력
-print(result_string)
+print(result)
