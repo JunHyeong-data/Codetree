c08874d187c3cf19e59da65e26d7d85d33080824
박준형 <parkjune0310@naver.com>
2025-08-25 00:39:27 +0900

    Title: 함수를 이용한 369 게임, Time: 92ms, Memory: 18MB, Status: Passed - Codetree

--- 250825/함수를 이용한 369 게임/369-games-using-functions.py
+++ 250825/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,15 +1,18 @@
 a, b = map(int, input().split())
 
 # Please write your code here.
-def fun(n):
-    return n // 10 == 3 or n //10 ==6 or n//10 ==9
+def count_369(n):
+    s = str(n)  # 숫자를 문자열로 바꿔서
+    for digit in s:  # 한 글자씩 확인
+        if digit in '369':
+            return True
 
-def func(n):
-    return n % 10 == 3 or n % 10 == 6 or n%10 ==9
+    return False
+    
 def game(a, b):
     cnt = 0
     for i in range(a, b+1):
-        if fun(i) or func(i) or i % 3 == 0:
+        if count_369(i) or i % 3 == 0:
             cnt += 1
     return cnt
 
